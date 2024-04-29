8fa82ad6dff6489bdef5d5a0e26e1f649b5fb88c
David Maas <d.maas+git@neurogears.org>
2024-04-29 18:08:22 -0500

    WIP: Implementation using escape codes.

--- Bonsai.Design/ObjectTextVisualizer.cs
+++ Bonsai.Design/ObjectTextVisualizer.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Windows.Forms;
 using Bonsai;
 using Bonsai.Design;
+using System.Diagnostics;
 using System.Drawing;
 using System.Reactive;
-using System.Text.RegularExpressions;
 using System.Text;
 
 [assembly: TypeVisualizer(typeof(ObjectTextVisualizer), Target = typeof(object))]
@@ -22,7 +22,7 @@ namespace Bonsai.Design
 
         RichTextBox textBox;
         UserControl textPanel;
-        Queue<char[]> buffer;
+        Queue<string> buffer;
         int bufferSize;
 
         /// <inheritdoc/>
@@ -31,51 +31,90 @@ namespace Bonsai.Design
         /// <inheritdoc/>
         protected override void ShowBuffer(IList<Timestamped<object>> values)
         {
-            if (values.Count > 0)
+            if (values.Count == 0)
+                return;
+
+            var sb = new StringBuilder();
+
+            // Add new values to the buffer (and only the ones which might appear)
+            for (int i = Math.Max(0, values.Count - bufferSize); i < values.Count; i++)
             {
-                base.ShowBuffer(values);
-                var sb = new StringBuilder();
-                foreach (var line in buffer)
-                {
-                    if (sb.Length > 0)
-                        sb.Append(Environment.NewLine);
-                    sb.Append(line);
-                }
-                textBox.Text = sb.ToString();
-                textPanel.Invalidate();
+                var rawText = values[i].Value?.ToString() ?? string.Empty;
+                sb.Clear();
+                sb.EnsureCapacity(rawText.Length + rawText.Length / 16); // Arbitrary extra space for control characters
+                MakeDisplayString(rawText, sb);
+                DisplayString(sb.ToString());
             }
+
+            // Update the visual representation of the buffer
+            Debug.Assert(buffer.Count <= bufferSize);
+            sb.Clear();
+            foreach (var line in buffer)
+            {
+                if (sb.Length > 0)
+                    sb.Append(Environment.NewLine);
+                sb.Append(line);
+            }
+            textBox.Text = sb.ToString();
+            textPanel.Invalidate();
         }
 
         /// <inheritdoc/>
         public override void Show(object value)
         {
-            value ??= string.Empty;
-            var rawText = value.ToString();
-            var text = new char[rawText.Length];
+            var rawText = value?.ToString() ?? string.Empty;
+            var stringBuilder = new StringBuilder(rawText.Length);
+            MakeDisplayString(rawText, stringBuilder);
+            DisplayString(stringBuilder.ToString());
+        }
 
-            for (int i = 0; i < text.Length; i++)
+        /// <summary>Converts the specified string for display in this visualizer</summary>
+        /// <param name="rawText">The raw text to be converted</param>
+        /// <param name="stringBuilder">The string builder which receives the display string</param>
+        protected virtual void MakeDisplayString(string rawText, StringBuilder stringBuilder)
+        {
+            foreach (char c in rawText)
             {
-                text[i] = rawText[i] switch
+                // To give point of reference we represent strings as they'd be represented in C# string literals
+                var replacement = c switch
                 {
-                    '\0' => '␀',
-                    '\n' => '␊',
-                    '\r' => '␍',
-                    //TODO: Other control characters
-                    char c => c,
+                    '\\' => @"\\", // Backslash
+                    '"' => "\\\"", // Double quote
+                    '\n' => @"\n", // Line feed
+                    '\r' => @"\r", // Carriage return
+                    '\x0085' => @"\x0085", // Next line character (NEL)
+                    '\x2028' => @"\x2028", // Unicode line separator
+                    '\x2029' => @"\x2029", // Unicode paragraph separator
+                    '\0' => @"\0", // Null
+                    '\a' => @"\a", // Alert
+                    '\b' => @"\b", // Backspace
+                    '\f' => @"\f", // Form feed
+                    '\t' => @"\t", // Tab
+                    '\v' => @"\v", // Vertical tab
+                    < ' ' => $@"\u{c:X4}", // Misc control characters
+                    _ => null,
                 };
+
+                if (replacement is null)
+                    stringBuilder.Append(c);
+                else
+                    stringBuilder.Append(replacement);
             }
-            
-            buffer.Enqueue(text);
-            while (buffer.Count > bufferSize)
-            {
+        }
+
+        private void DisplayString(string value)
+        {
+            // Loop instead of simple if because the buffer size may have decreased
+            while (buffer.Count >= bufferSize)
                 buffer.Dequeue();
-            }
+
+            buffer.Enqueue(value);
         }
 
         /// <inheritdoc/>
         public override void Load(IServiceProvider provider)
         {
-            buffer = new Queue<char[]>();
+            buffer = new Queue<string>();
             textBox = new RichTextLabel { Dock = DockStyle.Fill };
             textBox.Multiline = true;
             textBox.WordWrap = false;
@@ -127,7 +166,9 @@ namespace Bonsai.Design
         {
             bufferSize = 0;
             textBox.Dispose();
+            textPanel.Dispose();
             textBox = null;
+            textPanel = null;
             buffer = null;
         }
     }
